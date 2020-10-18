039804265318b9f94d49e6a6a908d26ed03d7ecd
Rafał Zubala <rzubala@gmail.com>
2020-10-18 20:26:16 +0200

    Updates

---
+++ precache-manifest.59b9ca787c2113f493f515a994f678e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f3a77fc0e29130ff69c235336afb0d",
+    "url": "/gift-drawing/index.html"
+  },
+  {
+    "revision": "ea665beb37ef8c323370",
+    "url": "/gift-drawing/static/css/main.ab7ee540.chunk.css"
+  },
+  {
+    "revision": "23e0acc93ba5a15fa748",
+    "url": "/gift-drawing/static/js/2.c408544d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gift-drawing/static/js/2.c408544d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea665beb37ef8c323370",
+    "url": "/gift-drawing/static/js/main.d45f2566.chunk.js"
+  },
+  {
+    "revision": "17b5bb5724c73c278410",
+    "url": "/gift-drawing/static/js/runtime-main.52d3cc4d.js"
+  }
+]);
