3b529911037ce8409ab4d5bd9f22a203945290ef
Rafał Zubala <rzubala@gmail.com>
2020-10-24 13:32:40 +0200

    Updates

---
+++ precache-manifest.f3c5fe03728847b4a6eebd32ef746bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fedae5bedcb984c362284c51d315ad",
+    "url": "/gift-draw/index.html"
+  },
+  {
+    "revision": "3030ca9f1e86391d40ed",
+    "url": "/gift-draw/static/css/main.266d0cb7.chunk.css"
+  },
+  {
+    "revision": "5d9b1eadb51a4187f0d3",
+    "url": "/gift-draw/static/js/2.aa462216.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gift-draw/static/js/2.aa462216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3030ca9f1e86391d40ed",
+    "url": "/gift-draw/static/js/main.9805ad5c.chunk.js"
+  },
+  {
+    "revision": "0c657c68da5b44397f83",
+    "url": "/gift-draw/static/js/runtime-main.4d24a32e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gift-drawing/precache-manifest.e83e2abc714be468ab241eb86d430c45.js"
+  "/gift-draw/precache-manifest.f3c5fe03728847b4a6eebd32ef746bf1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gift-drawing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gift-draw/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
